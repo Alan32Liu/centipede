4af1232245b70e6144d04fed66fa69278aaea6cb
Alan32Liu <donggeliu@google.com>
2022-07-26 13:02:51 +1000

    Use the official source of clang-14

--- install_dependencies_debian.sh
+++ install_dependencies_debian.sh
@@ -40,8 +40,8 @@ apt install -y git bazel binutils libssl-dev
 
 # Get clang-14, the oldest version that supports dataflow tracing
 mkdir /clang
-curl https://commondatastorage.googleapis.com/chromium-browser-clang/Linux_x64/clang-llvmorg-14-init-9436-g65120988-1.tgz -o /clang-14.tgz
-tar zxvf /clang-14.tgz -C /clang
+curl https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz -o /clang-14.tar.xz
+tar -xf /clang-14.tar.xz -C /clang
 
 # * TODO(kcc): llvm-symbolizer is required for running Centipede.
 #   it comes with clang, but may be called e.g. llvm-symbolizer-11
